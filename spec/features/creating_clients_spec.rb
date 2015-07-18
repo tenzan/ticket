f5336cff7dad6f83171b9ca3bacbc5db312adbea
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-19 00:06:56 +0900

    Add title functionality for client show page

--- spec/features/creating_clients_spec.rb
+++ spec/features/creating_clients_spec.rb
@@ -11,5 +11,11 @@ RSpec.feature 'Users can create new clients' do
     click_button 'Create Client'
     
     expect(page).to have_content 'Client has been created.'
+    
+    client = Client.find_by(name: 'Microsoft')
+    expect(page.current_url).to eq client_url(client)
+    
+    title = 'Microsoft - Clients - Ticketing System'
+    expect(page).to have_title title
   end
 end
