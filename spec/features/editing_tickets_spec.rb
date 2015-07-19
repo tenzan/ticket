7f7fee0c64c0d1294fab22fa2f0214206238fad6
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-19 23:03:01 +0900

    Tickets can now be edited

---
+++ spec/features/editing_tickets_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Users can edit existing tickets" do
+  let(:client) { FactoryGirl.create(:client) }
+  let(:ticket)  { FactoryGirl.create(:ticket, client: client) }
+
+  before do
+    visit client_ticket_path(client, ticket)
+    click_link "Edit Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Subject", with: "Windows OS"
+    click_button "Update Ticket"
+
+    expect(page).to have_content "Ticket has been updated."
+
+    within("#ticket h2") do
+      expect(page).to have_content "Windows OS"
+      expect(page).not_to have_content ticket.subject
+    end
+  end
+
+  scenario "with invalid attributes" do
+    fill_in "Subject", with: ""
+    click_button "Update Ticket"
+    
+    expect(page).to have_content "Ticket has not been updated."
+  end
+end
