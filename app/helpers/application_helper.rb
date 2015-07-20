0c3c222051280c7e03d861857f2d3465d2ab3817
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-20 14:17:57 +0900

    Only admins can see the links to create and delete clients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
+
   def title(*parts)
     unless parts.empty?
       content_for :title do
---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.feature "Users can only see the appropriate links" do
+  let(:client) { FactoryGirl.create(:client) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Client link" do
+      visit "/"
+      expect(page).not_to have_link "New Client"
+    end
+    scenario "cannot see the Delete Client link" do
+      visit client_path(client)
+      expect(page).not_to have_link "Delete Client"
+    end
+  end
+
+  context "regular users" do
+    before { login_as(user) }
+    scenario "cannot see the New Client link" do
+      visit "/"
+      expect(page).not_to have_link "New Client"
+    end
+    scenario "cannot see the Delete Client link" do
+      visit client_path(client)
+      expect(page).not_to have_link "Delete Client"
+    end
+  end
+
+  context "admin users" do
+    before { login_as(admin) }
+    scenario "can see the New Client link" do
+      visit "/"
+      expect(page).to have_link "New Client"
+    end
+    scenario "can see the Delete Client link" do
+      visit client_path(client)
+      expect(page).to have_link "Delete Client"
+    end
+  end
+
+end
