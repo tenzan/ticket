7755e0a64986dbc23b3ada0a8cf24d55ad61bdd7
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-20 12:07:36 +0900

    Adding admin flag to User model and seeding with default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.exists?(email: "admin@example.com")
+  User.create!(email: "admin@example.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@example.com")
+  User.create!(email: "viewer@example.com", password: "password")
+end
+
+["Microsoft", "Sun Microsystems"].each do |name|
+     unless Client.exists?(name: name)
+       Client.create!(name: name, description: "A sample project about #{name}"
+        )
+end end
--- spec/features/creating_clients_spec.rb
+++ spec/features/creating_clients_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 RSpec.feature 'Users can create new clients' do
   before do
+    login_as(FactoryGirl.create(:user, :admin))
     visit '/'
-    
+
     click_link 'New Client'
   end
   scenario 'with valid attributes' do
     fill_in 'Name', with: 'Microsoft'
     fill_in 'Description', with: 'Software developer'
     click_button 'Create Client'
-    
+
     expect(page).to have_content 'Client has been created.'
-    
+
     client = Client.find_by(name: 'Microsoft')
     expect(page.current_url).to eq client_url(client)
-    
+
     title = 'Microsoft - Clients - Ticketing System'
     expect(page).to have_title title
   end
-  
+
   scenario 'when providing invalid attributes' do
     click_button 'Create Client'
-    
+
     expect(page).to have_content 'Client has not been created.'
     expect(page).to have_content "Name can't be blank"
   end
