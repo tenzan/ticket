7bde6726b5dd6274fd19a1465b1fb74be88b270f
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-07-24 16:51:06 +0900

    Show the 'Edit Client' button only if the user has access

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,43 +1,55 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Users can only see the appropriate links" do
+RSpec.feature 'Users can only see the appropriate links' do
   let(:client) { FactoryGirl.create(:client) }
   let(:user)  { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:user, :admin) }
 
-  context "anonymous users" do
-    scenario "cannot see the New Client link" do
-      visit "/"
-      expect(page).not_to have_link "New Client"
+  context 'anonymous users' do
+    scenario 'cannot see the New Client link' do
+      visit '/'
+      expect(page).not_to have_link 'New Client'
     end
   end
 
-  context "non-admin users (client viewers)" do
+  context 'non-admin users (client viewers)' do
     before do 
       login_as(user)
       assign_role!(user, :viewer, client)
     end
 
-    scenario "cannot see the New Client link" do
-      visit "/"
-      expect(page).not_to have_link "New Client"
+    scenario 'cannot see the New Client link' do
+      visit '/'
+      expect(page).not_to have_link 'New Client'
     end
 
-    scenario "cannot see the Delete Client link" do
+    scenario 'cannot see the Delete Client link' do
       visit client_path(client)
-      expect(page).not_to have_link "Delete Client"
+      expect(page).not_to have_link 'Delete Client'
+    end
+
+    scenario 'cannot see the Edit Client link' do
+      visit client_path(client)
+      expect(page).not_to have_link 'Edit Client'
     end
   end
 
-  context "admin users" do
+  context 'admin users' do
     before { login_as(admin) }
-    scenario "can see the New Client link" do
-      visit "/"
-      expect(page).to have_link "New Client"
+
+    scenario 'can see the New Client link' do
+      visit '/'
+      expect(page).to have_link 'New Client'
     end
-    scenario "can see the Delete Client link" do
+
+    scenario 'can see the Delete Client link' do
       visit client_path(client)
-      expect(page).to have_link "Delete Client"
+      expect(page).to have_link 'Delete Client'
+    end
+
+    scenario 'can see the Edit Client link' do
+      visit client_path(client)
+      expect(page).to have_link 'Edit Client'
     end
   end
 
