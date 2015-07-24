9b6ca21aef4af39663d9e1443c3e62dfeddea207
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-07-24 12:01:56 +0900

    Handle authorization errors gracefully

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe ClientsController, type: :controller do
     message = 'The client you were looking for could not be found'
     expect(flash[:alert]).to eq message
   end
+
+  it 'handles permission errors by redirecting to a safe place' do
+    allow(controller).to receive(:current_user)
+
+    client = FactoryGirl.create(:client)
+    get :show, id: client
+
+    expect(response).to redirect_to(root_path)
+    message = "You aren't allowed to do that."
+    expect(flash[:alert]).to eq message
+  end
 end
